843667355c144aac799cb53a6e3a4bd9e2d03e67
gonza <gonza@gonza-maquinita>
2020-03-28 15:36:55 -0300

    pom change for war package - test for disable web server apache incorp.

--- demo/src/main/java/com/example/DreamTeam/demo/TestingWebApplication.java
+++ demo/src/main/java/com/example/DreamTeam/demo/TestingWebApplication.java
@@ -2,11 +2,19 @@ package com.example.DreamTeam.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class TestingWebApplication {
+public class TestingWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingWebApplication.class, args);
+	 }	
+		 @Override
+		    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		        return builder.sources(TestingWebApplication.class);
+		    }
 	}
-}
+
